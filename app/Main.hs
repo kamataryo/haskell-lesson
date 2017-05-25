2f731f73830a76b67a2aa7ba8009f05d98f111ef
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-05-25 16:34:10 +0900

    Add some

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+-- import Lib
+
+-- fibonacci :: integer -> integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
+
+secsToWeeks secs = let
+  perMinute = 60
+  perHour = 60 * perMinute
+  perDay = 24 * perHour
+  perWeek = 7 * perDay
+  in secs / perWeek
 
 main :: IO ()
-main = print "Hello, Haskell"
+main = do
+  putStrLn "Wha is fibo 5 ?"
+  x <- readLn
+  if x == fibonacci 5
+    then putStrLn "right!"
+    else putStrLn "wrong!"
